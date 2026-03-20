972fe6116cd4abbc95c8de8555c452ace571a118
pulpfictious <singhaman1342@gmail.com>
2026-03-20 12:47:41 +0530

    Added user input for elevator requests

--- src/elevator/main/Main.java
+++ src/elevator/main/Main.java
@@ -1,7 +1,8 @@
 package elevator.main;
 
-import elevator.model.Elevator;
 import elevator.controller.ElevatorController;
+import elevator.model.Elevator;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,9 +11,18 @@ public class Main {
         Elevator elevator = new Elevator();
         ElevatorController controller = new ElevatorController(elevator);
 
-        controller.requestFloor(5);
-        controller.requestFloor(2);
-        controller.requestFloor(7);
+        Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter number of requests: ");
+int n = sc.nextInt();
+
+for (int i = 0; i < n; i++) {
+    System.out.print("Enter floor: ");
+    int f = sc.nextInt();
+    controller.requestFloor(f);
+}
+
+sc.close();
 
     }
 }
